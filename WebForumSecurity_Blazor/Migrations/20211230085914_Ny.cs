a325ffe95bbca45e5fc8ae6657d9bed26f1cb6b5
Mattias Karlsson <mattias.karlsson13@learnet.se>
2022-01-02 12:38:03 +0100

    DontKnow

---
+++ WebForumSecurity_Blazor/Migrations/20211230085914_Ny.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebForumSecurity_Blazor.Migrations
+{
+    public partial class Ny : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TESTFile");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TESTFile",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TESTFile", x => x.Id);
+                });
+        }
+    }
+}
